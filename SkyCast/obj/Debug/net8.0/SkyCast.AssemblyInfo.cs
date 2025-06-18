649038871b1331f1ef9a7b6689acaed3ba9ac9e0
jaramirezma <jaramirezma@megacable.com.mx>
2025-06-18 10:14:21 -0600

    Clase 27 | C#

--- SkyCast/obj/Debug/net8.0/SkyCast.AssemblyInfo.cs
+++ SkyCast/obj/Debug/net8.0/SkyCast.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkyCast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e7be4c3394ecde351cb860b63cfad0f1a5a74a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df02d35b181e6e2a2834591ea8525689a06edc29")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkyCast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkyCast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
